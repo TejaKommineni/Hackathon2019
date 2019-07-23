5179a00b4dcc63e89416cae2af22915eb8137ae8
Teja Kommineni <tekommin@microsoft.com>
2019-07-22 22:55:56 -0700

    Adding Redux functionality to app

--- CapacityManagement-Frontend/src/Regional.js
+++ CapacityManagement-Frontend/src/Regional.js
@@ -1,21 +1,47 @@
 import React from "react";
 import Map from "./Map";
 import TopNavbar from "./TopNavbar";
+import SideNavbar from "./SideNavbar";
 import {Container , Row, Col } from 'react-bootstrap';
+import { connect } from 'react-redux'
 import './Global.css';
+import { selectGeography } from './redux/Actions'
 
-export default class Regional extends React.Component {
+class Regional extends React.Component {
 
+    
 	render() {
+        console.log(this.props.selectedGeography);
 		return (				
 			<Container fluid={true}  style={{ paddingLeft: 0, paddingRight: 0, height: "100%" }} >
+            <Row> {this.props.accordionOpen}</Row>
 			<Row>
 			  <Col><TopNavbar/></Col>
 			</Row>
             <Row style={{ paddingLeft: 0, paddingRight: 0, height: "100%" }}>
-              <Col><Map/></Col>
+			  <Col xs={2}>
+                  <SideNavbar 
+                  selectedGeography={this.props.selectedGeography}
+                  selectGeography={this.props.selectGeography}
+                  /></Col>
+        <Col><Map/></Col>
 			</Row>
 		  </Container>
 		);
 	}
 }
+
+const mapStateToProps = state => ({
+    selectedGeography: state.regional.selectedGeography
+  })
+  
+  const mapDispatchToProps = dispatch => ({
+    selectGeography: selectedGeography => dispatch(selectGeography(selectedGeography))
+  })
+  
+const RegionalContainer =  connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Regional)
+
+export default RegionalContainer;
--- CapacityManagement-Frontend/src/index.js
+++ CapacityManagement-Frontend/src/index.js
@@ -5,21 +5,45 @@ import './index.css';
 import App from './App';
 import Geographical from './Geographical';
 import Global from './Global';
-import Regional from './Regional';
+import RegionalContainer from './Regional';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import capacityManagementApp from './redux/Reducers'
+import {
+  selectGeography,
+  selectRegion,
+} from './redux/Actions'
+const store = createStore(capacityManagementApp)
 
+
+
+// Log the initial state
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// Dispatch some actions
+//store.dispatch(selectGeography('Learn about actions'))
+//store.dispatch(selectRegion('Learn about reducers'))
+
+// Stop listening to state updates
+unsubscribe()
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
+  <Provider store={store}>
     <Router>
       <div style={{ height: "100%" }}>
         <Route exact path="/" component={App} />
         <Route path="/global" component={Global} />
         <Route path="/geographical/:id" component={Geographical} />
-        <Route path="/regional" component={Regional} />
+        <Route path="/regional" component={RegionalContainer} />
       </div>
-    </Router>, rootElement  
+    </Router>
+  </Provider>, rootElement  
 );
 
 
