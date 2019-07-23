77a8e772a92f5c3e9eff7cb87ddd9669e1aac02a
Harika Reddy Kumbum <hakumbu@microsoft.com>
2019-07-23 15:38:49 -0700

    Removing horizontal and vertical scrolling

--- CapacityManagement-Frontend/src/Global.js
+++ CapacityManagement-Frontend/src/Global.js
@@ -8,7 +8,7 @@ export default class Global extends React.Component {
 
 	render() {
 		return (				
-			<Container fluid={true}  style={{ paddingLeft: 0, paddingRight: 0, height: "100%" }} >
+			<Container fluid = {true}  style={{paddingLeft: 0, paddingRight: 0, height: "100%"}} >
 			<Row style={{height: "10%", margin: 0}}>
 			  <Col style={{paddingLeft: 0, paddingRight: 0 }}><TopNavbar/></Col>
 			</Row>
